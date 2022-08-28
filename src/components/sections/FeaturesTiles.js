f71c1c26e79f931c2c3d6a31c15a02381ed49f3b
Víctor Tamayo <victortamayo509@gmail.com>
2022-08-28 11:50:04 -0500

    first

---
+++ src/components/sections/FeaturesTiles.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionTilesProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
+const propTypes = {
+  ...SectionTilesProps.types
+}
+
+const defaultProps = {
+  ...SectionTilesProps.defaults
+}
+const FeaturesTiles = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-tiles section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-tiles-inner section-inner pt-0',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const tilesClasses = classNames(
+    'tiles-wrap center-content',
+    pushLeft && 'push-left'
+  );
+
+  const sectionHeader = {
+    title: '¿Quiénes somos?',
+    paragraph: 'ContApp es una aplicación y programa de naturaleza financiera-Contable; Que fungirá como intermediario en los servicios de contabilidad de Personas Físicas y Morales. Conectando Clientes con Contadores profesionales capacitados  que les brindarán el servicio de manera clara, puntual y correcta. Por un lapso de tiempo determinado.'
+  };
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/camera.svg')}
+                      alt="Features tile icon 06"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Toma una foto y nosotros hacemos el resto
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Atravez de una foto de tu recibo puedes facilitar los procesos contables que normalmente tardan algun tiempo en resolverse. Toma la foto y nostros nos encargamos de la automatización de la facturación.
+                    </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+FeaturesTiles.propTypes = propTypes;
+FeaturesTiles.defaultProps = defaultProps;
+
+export default FeaturesTiles;
